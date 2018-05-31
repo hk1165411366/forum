dac7259019e2360f17a18057d50c4da4dc0506b8
vagrant <vagrant@homestead>
2018-05-31 13:00:36 +0000

    two push

--- app/Http/Controllers/ThreadsController.php
+++ app/Http/Controllers/ThreadsController.php
@@ -35,9 +35,12 @@ class ThreadsController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(Thread $thread)
     {
-        //
+        $thread->addReply([
+            'body' => request('body'),
+            'user_id' => auth()->id()
+        ]);
     }
 
     /**
--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -5,6 +5,8 @@
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
                     <div class="panel-heading">
+                        <a href="">{{ $thread->creator->name }}</a>
+                          &nbsp; &nbsp;发表了:
                         <h4>{{ $thread->title }}</h4>
                     </div>
 
